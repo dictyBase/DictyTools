a1c31c3f7d846233f72bf5ff9ea1a714245ce3a8
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-04-11 16:32:19 -0500

    separate logic for getting sequences for other genomes discoideum now considered to be special case No longer uses Bio::SeqIO to just format the fasta sequence

--- lib/DictyTools/Controller/Fasta.pm
+++ lib/DictyTools/Controller/Fasta.pm
@@ -19,36 +19,32 @@ sub write_sequence {
     my $organism   = $self->req->param('organism');
     my $connection = $self->get_model($organism);
 
-    my $feature =
-        $connection->resultset('Sequence::Feature')
+    my $feature = $connection->resultset('Sequence::Feature')
         ->find( { 'dbxref.accession' => $id }, { join => 'dbxref' } );
 
-    my $sequence = $self->get_sequence( $feature, $type );
-    my $header = $self->get_header( $feature, $type );
-
-    if ( !$sequence ) {
-        $self->render( text =>
-                "No sequence of type $type found for $id, please report to "
-                . $self->app->config->{blast}->{site_admin_email} );
-        return;
+    my ( $sequence, $header );
+    if ( $organism eq 'discoideum' ) {
+        $sequence = $self->get_sequence( $feature, $type );
+        $header   = $self->get_header( $feature,   $type );
+        if ( !$sequence ) {
+            $self->render( text =>
+                    "No sequence of type $type found for $id, please report to "
+                    . $self->app->config->{blast}->{site_admin_email} );
+            return;
+        }
     }
-    my $str = IO::String->new;
-    eval {
-        my $seqobj = Bio::Seq->new(
-            -display_id => $id,
-            -desc       => $header,
-            -seq        => $sequence
-        );
-
-        my $out = Bio::SeqIO->new( -format => 'fasta', -fh => $str );
-        $out->write_seq($seqobj);
-    };
-    if ($@) {
-        $self->app->log->debug( "Eror writing fasta sequence for " 
-                . $id . "\n"
-                . "header: $header\n$@" );
+    else {
+		($header, $sequence) = $self->get_sequence_info($feature, $type);
+        if ( !$sequence ) {
+            $self->render( text =>
+                    "No sequence of type $type found for $id, please report to "
+                    . $self->app->config->{blast}->{site_admin_email} );
+            return;
+        }
     }
-    $self->render( text => ${ $str->string_ref } );
+    $sequence =~ s/(\w{1,60})/$1\n/g;
+    my $fasta = ">$header\n$sequence";
+    $self->render( text => $fasta );
 }
 
 1;
