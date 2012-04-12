1ed4fa2093a1ded69aa12d8c2f0820ec60fbb6bd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-04-12 16:55:26 -0500

    Added separate method to get sequence for other genome

--- lib/DictyTools/Controller/Fasta.pm
+++ lib/DictyTools/Controller/Fasta.pm
@@ -34,7 +34,8 @@ sub write_sequence {
         }
     }
     else {
-		($header, $sequence) = $self->get_sequence_info($feature, $type);
+        ( $header, $sequence )
+            = $self->sequence_info_from_chado( $feature, $type );
         if ( !$sequence ) {
             $self->render( text =>
                     "No sequence of type $type found for $id, please report to "
@@ -45,6 +46,60 @@ sub write_sequence {
     $sequence =~ s/(\w{1,60})/$1\n/g;
     my $fasta = ">$header\n$sequence";
     $self->render( text => $fasta );
+}
+
+sub sequence_info_from_chado {
+    my ( $self, $feature, $type ) = @_;
+
+    # -- direct sequence retrieval no fallbacks at this point
+    if ( $type =~ /Genomic/i ) {    #unspliced transcript sequence
+        return ( $feature->dbxref->accession, $feature->residues );
+    }
+    elsif ( $type =~ /coding/i ) {
+        my $exon_rs = $feature->search_related(
+            'feature_relationship_objects',
+            { 'type.name' => 'part_of' },
+            { join        => 'type' }
+            )->search_related(
+            'subject',
+            { 'type_2.name' => 'exon' },
+            { join          => 'type' }
+            )
+            ->search_related( 'featureloc_features', {},
+            { 'order_by' => { -asc => 'fmin' } } );
+
+        my $seq;
+        for my $erow ( $exon_rs->all ) {
+            my $start  = $erow->fmin + 1;
+            my $end    = $erow->fmax;
+            my $seqlen = $end - $start + 1;
+            $seq .= $erow->search_related(
+                'srcfeature',
+                {},
+                {   select => [ \"SUBSTR(me.residues,  $start, $seqlen)" ],
+                    as     => 'fseq'
+                }
+            )->first->get_column('fseq');
+        }
+        if ( $dbrow->featureloc_features->first->strand == -1 ) {
+            $seq = join( '', reverse( split '', $seq ) );
+            $seq =~ tr/ATGC/TACG/;
+        }
+        return ( $feature->dbxref->accession, $seq );
+    }
+    else {
+        my $poly_rs = $feature->search_related(
+            'feature_relationship_objects',
+            { 'type.name' => 'part_of' },
+            { join        => 'type' }
+            )->search_related(
+            'subject',
+            { 'type_2.name' => 'polypeptide' },
+            { join          => 'type' }
+            );
+        my $row = $poly_rs->first;
+        return ($row->dbxref->accession, $row->residues);
+    }
 }
 
 1;
