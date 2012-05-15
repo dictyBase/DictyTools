2f66ca28c133d7ae6ef5bdf52e4c6414f3e8a270
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-05-15 11:43:40 -0500

    Refactored index action
    Identifier prefixes by default are generated from database but can be overriden in the
    config file. The display name are determined from the genus and species names of the
    organism.

--- lib/DictyTools/Controller/Organism.pm
+++ lib/DictyTools/Controller/Organism.pm
@@ -1,47 +1,24 @@
 package DictyTools::Controller::Organism;
 
 use strict;
-use warnings;
-
 use base 'Mojolicious::Controller';
-
-use version;
-our $VERSION = qv('2.0.0');
 
 sub index {
     my ($self) = @_;
-    my $organism = $self->app->config->{organism};
-
+    my $config = $self->app->config->{organism};
     my $array;
-    for my $name ( keys %$organism ) {
-        my $hash;
-        my $common_name
-            = $organism->{$name}->{common_name}
-            ? $organism->{$name}->{common_name}
-            : $name;
-
-        # have to get rid of repitition somehow
-        if ( defined $organism->{$name}->{display} ) {
-            $hash->{display} = $organism->{$name}->{display};
+    for my $organism ( $self->organisms_in_db ) {
+        my $prefix = $organism->identifier_prefix;
+        if (defined $config->{ $organism->common_name }->{identifier_prefix} )
+        {
+            $prefix
+                = $config->{ $organism->common_name }->{identifier_prefix};
         }
-        elsif ( defined $organism->{$name}->{process} ) {
-            my $org
-                = $self->get_model($organism)->resultset('Organism::Organism')
-                ->search( { 'common_name' => $common_name } )->first;
-            my ($species) = ( ( split /\s+/, $org->species ) )[0];
-            $hash->{display} = $org->genus . ' ' . $species;
-        }
-        else {
-            my $org
-                = $self->get_model($organism)->resultset('Organism::Organism')
-                ->search( { 'common_name' => $common_name } )->first;
-            $hash->{display} = $org->genus . ' ' . $org->species;
-        }
-        for my $val (qw/taxon_id identifier_prefix site_url/) {
-            $hash->{$val} = $organism->{$name}->{$val}
-                if defined $organism->{$name}->{$val};
-        }
-        push @$array, $hash;
+        push @$array,
+            {
+            display           => $organism->display_name,
+            identifier_prefix => $prefix
+            };
     }
     $self->render( json => $array );
 }
