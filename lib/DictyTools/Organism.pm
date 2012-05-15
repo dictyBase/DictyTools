6fc8ee1d302450004eb85131a42fb159d55efa14
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-05-14 20:37:01 -0500

    added identifier_prefix attribute

--- lib/DictyTools/Organism.pm
+++ lib/DictyTools/Organism.pm
@@ -6,7 +6,7 @@ use Mojo::Base -base;
 
 # Module implementation
 #
-has [qw/common_name genus species name_for_display/];
+has [qw/common_name genus species name_for_display identifier_prefix/];
 
 1;    # Magic true value required at end of module
 
