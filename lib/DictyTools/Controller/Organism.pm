1cdb4eae45666c1fe97fe200fab4f2927d870f35
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-05-17 16:13:03 -0500

    Added common_name, genus and species attributes in the json output

--- lib/DictyTools/Controller/Organism.pm
+++ lib/DictyTools/Controller/Organism.pm
@@ -8,8 +8,9 @@ sub index {
     my $config = $self->app->config->{organism};
     my $array;
     for my $organism ( $self->organisms_in_db ) {
-    	# skip if given in the config
-    	next if exists $config->{$organism->common_name}->{skip};
+
+        # skip if given in the config
+        next if exists $config->{ $organism->common_name }->{skip};
 
         my $prefix = $organism->identifier_prefix;
         if (defined $config->{ $organism->common_name }->{identifier_prefix} )
@@ -19,7 +20,10 @@ sub index {
         }
         push @$array,
             {
-            display           => $organism->name_for_display,
+            display => $organism->name_for_display,
+            species => $organism->species,
+            genus   => $organism->genus,
+            common_name => $organism->common_name,
             identifier_prefix => $prefix
             };
     }
